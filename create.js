d490540a9113f08b1b392cf7e97950e2c5399e93
pb3975 <metzwa.2@gmail.com>
2018-06-08 11:28:05 -0400

    FIxing bug

--- create.js
+++ create.js
@@ -2,7 +2,6 @@ import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
-gibberish.what;
 export async function main(event, context, callback) {
   const data = JSON.parse(event.body);
   const params = {
